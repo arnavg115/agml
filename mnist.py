226c7172455cb72ecb1104336d32d671aac6363b
Arnav G <73305591+arnavg115@users.noreply.github.com>
2023-03-29 23:49:55 -0700

    added final changes

--- mnist.py
+++ mnist.py
@@ -9,22 +9,26 @@ from snn.vis import display_mnist_image
 import urllib.request
 
 
-download_data = False
-download_weights = False
+download_data = True
+download_weights = True
 
-def download(url):
 
-    urllib.request.urlretrieve(url, 'train.csv')
     
-
 if download_data:
-    download("https://github.com/arnavg115/snn/releases/download/DATA/train.csv")
+  print("Downloading MNIST Data")
+  url = "https://github.com/arnavg115/snn/releases/download/DATA/train.csv"
+  urllib.request.urlretrieve(url, 'train.csv')
 if download_weights:
-    download("https://github.com/arnavg115/snn/releases/download/DATA/model.pkl")
+  print("Downloading pretrained model weights")
+  url= "https://github.com/arnavg115/snn/releases/download/DATA/model.pkl"
+  filename = 'model.pkl'
+  with urllib.request.urlopen(url) as response, open(filename, 'wb') as out_file:
+      data = response.read()  # read the file contents in memory
+      out_file.write(data) 
 
 
 out = []
-
+print("Opening Data")
 with open('train.csv') as csvfile:
     reader = csv.reader(csvfile)
     out = list(reader)
@@ -45,6 +49,7 @@ else:
 
 
 
+print("Starting inference")
 while True:
     ins = input("Enter index of image to see and predict (0 - 9999), or enter q to quit: ")
     if ins == "q":
